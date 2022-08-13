a2b47f1525a6cbf55dff8b759416fc7039a4599f
MikhailYuzhakov <miha.yuzhakov@gmail.com>
2022-08-13 20:32:07 +0300

    Введение в Big O Notation

---
+++ Boot3/Program.cs
@@ -0,0 +1,27 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int [, ] matrix = new int[n,n];
+
+for (int i = 0; i < n; i++) 
+{
+    for (int j = i; j < n; j++)
+    {
+            matrix[i,j] = (i + 1) * (j + 1);
+            matrix[j,i] = (j + 1) * (i + 1);
+    }
+}
+
+for (int i = 0; i < n; i++) 
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(matrix[i,j]);
+        Console.Write("\t");
+    }
+    Console.WriteLine();
+}
+
+// было со стандартным перемножением скорость алгоритма O(n^2)
+// стало с модифицированным алгоритмом скорость O(n^2 / 2)
+
+
+
